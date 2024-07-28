783ff92d0ccec8e381a7380735570e91638a8c32
itzParsaYC <115403383+ParsaAslaniYC@users.noreply.github.com>
2024-07-28 12:27:30 +0330

    topaz: lunch: Fixing lunch entry

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -23,9 +23,9 @@ COMMON_LUNCH_CHOICES := \
     aosp_topaz-trunk_staging-userdebug \
     aosp_topaz-trunk_staging-user \
     aosp_topaz-trunk_staging-eng \
-    aosp_topaz_64-ap2a-userdebug \
-    aosp_topaz_64-ap2a-user \
-    aosp_topaz_64-ap2a-eng \
+    aosp_topaz64-ap2a-userdebug \
+    aosp_topaz64-ap2a-user \
+    aosp_topaz64-ap2a-eng \
     aosp_topaz_64bit_zygote-ap2a-userdebug \
     aosp_topaz_64bit_zygote-ap2a-user \
     aosp_topaz_64bit_zygote-ap2a-eng \
