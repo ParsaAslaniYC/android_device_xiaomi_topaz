7bd8e86140a8752d6d3121f808b89d8822e264f8
itzParsaYC <115403383+ParsaAslaniYC@users.noreply.github.com>
2024-08-01 18:18:09 +0330

    topaz: welcome derpfest!, take a sit over there

--- derp_topaz64.mk
+++ derp_topaz64.mk
@@ -1,6 +1,6 @@
-$(call inherit-product, device/xiaomi/topaz/aosp_topaz.mk)
+$(call inherit-product, device/xiaomi/topaz/derp_topaz.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
 # Device name etc
-PRODUCT_NAME := aosp_topaz64
+PRODUCT_NAME := derp_topaz64
 PRODUCT_MODEL := Redmi Note 12 4G
