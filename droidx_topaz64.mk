6f7e711c2a80ec967933b5a8e6c2faf10c2405cf
itzParsaYC <115403383+ParsaAslaniYC@users.noreply.github.com>
2024-08-03 12:26:31 +0330

    topaz: Welcome Droid-X UI

--- droidx_topaz64.mk
+++ droidx_topaz64.mk
@@ -2,5 +2,5 @@ $(call inherit-product, device/xiaomi/topaz/aosp_topaz.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
 # Device name etc
-PRODUCT_NAME := aosp_topaz64
+PRODUCT_NAME := droidx_topaz64
 PRODUCT_MODEL := Redmi Note 12 4G
